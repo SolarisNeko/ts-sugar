24f1be5430cc752c84ca2d73172fb2393df11de4
SolarisNeko <1417015340@qq.com>
2023-11-06 22:01:51 +0800

    update

--- src/time/DateTimeUtils.ts
+++ src/time/DateTimeUtils.ts
@@ -1,13 +1,7 @@
-import AbstractSingleton from "../core/AbstractSingleton";
-
-export default class DateTimeUtils extends AbstractSingleton {
+export default class DateTimeUtils {
 
     //  时区：暂时按东八区，需要与服务器一致
     public static readonly TIME_ZONE: string = "+08";
-
-    public constructor() {
-        super();
-    }
 
     /**
      /**
@@ -15,7 +9,7 @@ export default class DateTimeUtils extends AbstractSingleton {
      * 表示时间点
      * yyyy-mm-dd hh:mm:ss
      */
-    public getDateTimeText(timeMs: number): string {
+    public static getDateTimeText(timeMs: number): string {
         var date: Date = new Date(timeMs);
         var year: number = date.getFullYear();
         var month: number = date.getMonth() + 1; 	//返回的月份从0-11；
@@ -29,8 +23,8 @@ export default class DateTimeUtils extends AbstractSingleton {
         return year + "-" + month + "-" + day + " " + hoursStr + ":" + minuteStr + ":" + secondStr;
     }
 
-    public getTomorrowStartTime(time: number = this.currentTimeMs,
-                                offsetHour: number = 0
+    public static getTomorrowStartTime(time: number = this.currentTimeMs,
+                                       offsetHour: number = 0
     ): number {
         let date: Date = new Date(time);
         return new Date(date.getFullYear(), date.getMonth(), date.getDate(),
@@ -39,8 +33,8 @@ export default class DateTimeUtils extends AbstractSingleton {
             .getTime() + 24 * 3600 * 1000;
     }
 
-    public getYesterdayStartTime(time: number = this.currentTimeMs,
-                                 offsetHour: number = 0
+    public static getYesterdayStartTime(time: number = this.currentTimeMs,
+                                        offsetHour: number = 0
     ): number {
         let date: Date = new Date(time);
         return new Date(date.getFullYear(), date.getMonth(), date.getDate(),
@@ -51,8 +45,8 @@ export default class DateTimeUtils extends AbstractSingleton {
      * 获取下次后端刷新数据时间点（自定义刷新时间）
      * 默认 0 点刷新
      */
-    public getRefreshDataTime(time: number = this.currentTimeMs,
-                              offsetHour: number = 0
+    public static getRefreshDataTime(time: number = this.currentTimeMs,
+                                     offsetHour: number = 0
     ): number {
         let date = new Date(time);
         if (date.getHours() < offsetHour) {
@@ -72,8 +66,8 @@ export default class DateTimeUtils extends AbstractSingleton {
      * 获取上次后端刷新数据时间点（自定义刷新时间）
      * 默认 0 点刷新
      */
-    public getLastRefreshDateTime(time: number = this.currentTimeMs,
-                                  offsetHour: number = 0
+    public static getLastRefreshDateTime(time: number = this.currentTimeMs,
+                                         offsetHour: number = 0
     ): number {
         let date = new Date(time);
         if (date.getHours() < offsetHour) {
@@ -90,7 +84,7 @@ export default class DateTimeUtils extends AbstractSingleton {
     }
 
 
-    public getMonthBySecond(time: number): number {
+    public static getMonthBySecond(time: number): number {
         var date: Date = new Date(time);
         var month: number = date.getMonth() + 1;
         return month;
@@ -99,7 +93,7 @@ export default class DateTimeUtils extends AbstractSingleton {
     /**
      * 客户端运行时间，仅用于防加速
      */
-    public get currentTimeMs(): number {
+    public static get currentTimeMs(): number {
         return new Date().getTime();
     }
 
--- test/time/DateTimeUtils.test.ts
+++ test/time/DateTimeUtils.test.ts
@@ -1,56 +1,50 @@
 import DateTimeUtils from "../../src/time/DateTimeUtils";
 
 describe('DateTimeUtils', () => {
-    let dateTimeUtils: DateTimeUtils;
-
-    beforeEach(() => {
-        dateTimeUtils = new DateTimeUtils();
-    });
-
     test('getDateTimeText should format date correctly', () => {
         const date = new Date('2023-10-25T13:30:00Z');
         const expectedFormattedDate = '2023-10-25 13:30:00';
-        const formattedDate = dateTimeUtils.getDateTimeText(date.getTime());
+        const formattedDate = DateTimeUtils.getDateTimeText(date.getTime());
         expect(formattedDate).toBe(expectedFormattedDate);
     });
 
-    test('getTomorrowStartTime should calculate the correct time', () => {
+    test('getTomorrowStartTime should calculate the correct time in timezone', () => {
         const mockDate = new Date('2023-10-25T13:30:00Z').getTime();
         const offsetHour = 8; // 8 AM
-        const tomorrowStartTime = dateTimeUtils.getTomorrowStartTime(mockDate, offsetHour);
+        const tomorrowStartTime = DateTimeUtils.getTomorrowStartTime(mockDate, offsetHour);
         const expectedTime = new Date('2023-10-26T08:00:00Z').getTime();
         expect(tomorrowStartTime).toBe(expectedTime);
     });
 
-    test('getYesterdayStartTime should calculate the correct time', () => {
+    test('getYesterdayStartTime should calculate the correct time in timezone', () => {
         const mockDate = new Date('2023-10-25T13:30:00Z').getTime();
         const offsetHour = 8; // 8 AM
-        const yesterdayStartTime = dateTimeUtils.getYesterdayStartTime(mockDate, offsetHour);
+        const yesterdayStartTime = DateTimeUtils.getYesterdayStartTime(mockDate, offsetHour);
         const expectedTime = new Date('2023-10-24T08:00:00Z').getTime();
         expect(yesterdayStartTime).toBe(expectedTime);
     });
 
-    test('getRefreshDataTime should return correct time based on offsetHour', () => {
+    test('getRefreshDataTime should return correct time based on offsetHour and timezone', () => {
         const mockDate = new Date('2023-10-25T13:30:00Z').getTime();
         const offsetHour = 10; // 10 AM
-        const refreshTime = dateTimeUtils.getRefreshDataTime(mockDate, offsetHour);
+        const refreshTime = DateTimeUtils.getRefreshDataTime(mockDate, offsetHour);
         const expectedTime = new Date('2023-10-26T10:00:00Z').getTime();
         expect(refreshTime).toBe(expectedTime);
     });
 
-    test('getLastRefreshDateTime should return correct time based on offsetHour', () => {
+    test('getLastRefreshDateTime should return correct time based on offsetHour and timezone', () => {
         const mockDate = new Date('2023-10-25T13:30:00Z').getTime();
         const offsetHour = 10; // 10 AM
-        const lastRefreshTime = dateTimeUtils.getLastRefreshDateTime(mockDate, offsetHour);
+        const lastRefreshTime = DateTimeUtils.getLastRefreshDateTime(mockDate, offsetHour);
         const expectedTime = new Date('2023-10-25T10:00:00Z').getTime();
         expect(lastRefreshTime).toBe(expectedTime);
     });
 
-    test('getMonthBySecond should return correct month', () => {
+    test('getMonthBySecond should return correct month in timezone', () => {
         const mockDate = new Date('2023-10-25T13:30:00Z').getTime();
-        const month = dateTimeUtils.getMonthBySecond(mockDate);
+        const month = DateTimeUtils.getMonthBySecond(mockDate);
         expect(month).toBe(10);
     });
 
-    // Add more test cases to cover other methods and edge cases...
+    // 以及其他方法的边界情况和时区问题的测试...
 });
