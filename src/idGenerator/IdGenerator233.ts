dc4ec1a2ac4f844b73c9ffac6f6435f2aaeb5142
solarisneko <1417015340@qq.com>
2024-05-30 14:55:07 +0800

    feat(IdGenerator233): 唯一id生成器2

--- src/idGenerator/IdGenerator233.ts
+++ src/idGenerator/IdGenerator233.ts
@@ -10,12 +10,12 @@ export class IdGenerator233 {
      * 获取一个全局唯一ID
      * @param type 类型
      */
-    static getNextUID(type: string = "default"): number {
+    static getNextUid(type: string = "default"): number {
         return IdGenerator233._typeToCounterMap.updateThenGet(type, 0, (value) => value + 1)
     }
 
     static getNextUidString(): string {
-        let guid: string = this.getNextUID().toString() + "-";
+        let guid: string = this.getNextUid().toString() + "-";
         for (let i = 1; i <= 32; i++) {
             let n = Math.floor(Math.random() * 16.0).toString(16);
             guid += n;
