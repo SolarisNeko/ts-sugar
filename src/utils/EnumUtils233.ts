722a1d4aa3df70c87933fc2b85812a13e0506cc2
solarisneko <1417015340@qq.com>
2024-06-11 16:42:15 +0800

    feat(enum工具)

--- src/utils/EnumUtils233.ts
+++ src/utils/EnumUtils233.ts
@@ -7,24 +7,35 @@
  */
 export class EnumUtils233 {
 
+// 存储枚举的反向映射表 <enumObj, Map<enumValue, enumKeyName>>
+    private static _enumValueToKeyMap = new Map<object, Map<string | number, string>>();
 
     /**
      * 根据 enum value(后端通信用) 获取 enum keyName(策划配置用)
-     * - 泛型约束
-     *
-     * @param enumObj 枚举对象
-     * @param value 枚举值
-     * @returns 枚举键名，如果找不到则返回 undefined
-     *
-     * @demo         const bigTalentName = EnumUtils.getEnumKeyNameByValue(ServerEnums.TalentType, ServerEnums.TalentType.ADVANCED);
      */
-    static getEnumKeyNameByValue<T extends { [key: string]: any }>(
+    static getEnumKeyNameByValue<T extends { [key: string]: string | number }>(
         enumObj: T,
         value: T[keyof T]
-    ): string | undefined {
+    ): string | null {
+        if (!value) {
+            return null;
+        }
 
-        return enumObj[value] as string;
+        // lazy init 反向 value to key map
+        let enumValueToKeyMap = this._enumValueToKeyMap.get(enumObj);
+        if (!enumValueToKeyMap) {
+            // 如果反向映射表不存在，则构建一个
+            enumValueToKeyMap = new Map<string | number, string>();
+            for (const key in enumObj) {
+                enumValueToKeyMap.set(enumObj[key], key);
+            }
+            this._enumValueToKeyMap.set(enumObj, enumValueToKeyMap);
+        }
+
+        // 直接从反向映射表中查找
+        return enumValueToKeyMap.get(value) ?? null;
     }
+
 
     /**
      * 获取枚举的所有键名
