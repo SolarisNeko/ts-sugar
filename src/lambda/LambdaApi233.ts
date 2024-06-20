e899e8ab26cbb3cf47f2ee8fade465a494326d1d
solarisneko <1417015340@qq.com>
2024-06-20 20:07:08 +0800

    lambda

---
+++ src/lambda/LambdaApi233.ts
@@ -0,0 +1,19 @@
+export namespace LambdaApi233 {
+
+    /**
+     * 消费函数
+     */
+    export type Consumer<T> = (value: T) => void;
+
+
+    /**
+     * 判断函数
+     */
+    export type Predicate<T> = (value: T) => void;
+
+
+    /**
+     * 合并函数
+     */
+    export type MergeFunction<T> = (value1: T, value2: T) => T;
+}
