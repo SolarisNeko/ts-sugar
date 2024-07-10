c735e592f90de0f048ac6dbedbb2541f4d5d42c4
solarisneko <1417015340@qq.com>
2024-07-10 21:11:10 +0800

    lambda

--- src/lambda/LambdaApi233.ts
+++ src/lambda/LambdaApi233.ts
@@ -19,4 +19,16 @@ export namespace LambdaApi233 {
      * 合并函数
      */
     export type MergeFunction<T> = (value1: T, value2: T) => T;
+
+    /**
+     * 空函数
+     */
+    export type VoidFunction = () => void;
+
+    /**
+     * 没有参数有返回值的函数
+     */
+    export type NoParamHaveReturnFunction<T> = () => T;
+
+
 }
